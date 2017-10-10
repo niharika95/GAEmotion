5606519716096b76f24a59034b0928dcaef0f1f7
Niharika <niharika13dalal@gmail.com>
2017-10-10 17:14:18 -0400

    Addded urls of the months. Bugs exist.

--- urlContentParser.py
+++ urlContentParser.py
@@ -3,12 +3,13 @@ from BeautifulSoup import BeautifulSoup
 
 opener = urllib2.build_opener()
 
-def urlContentParser(urlList):
+def urlContentParser(urlList, eachMonth):
     for eachUrl in enumerate(urlList):
+        print ("Processing file "+ str(eachUrl[0]+1) + "...")
         ourUrl = opener.open(eachUrl[1]).read()
         soup = BeautifulSoup(ourUrl)
         spanList = soup.findAll('span')
-        outFile = open('C://Users//lenovo//PycharmProjects//GAEmotion//Result//' + str(eachUrl[0]+1) + '.txt','w')
+        outFile = open('C://Users//lenovo//PycharmProjects//GAEmotion//Result//' + str(eachMonth+1) + "//" + str(eachUrl[0]+1) + '.txt','w')
 
         for eachSpan in spanList:
             if (eachSpan['class'] == "d"):
--- urlParser.py
+++ urlParser.py
@@ -2,30 +2,67 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 import urlContentParser
 
+# 2016-17
+# October - 1475272800
+# November - 1477954800
+# December - 1480546800
+# January - 1483225200
+# February - 1485903600
+# March - 1488322800
+# April - 1490997600
+# May - 1493589600
+# June - 1496268000
+# July - 1498860000
+# August - 1501538400
+# September - 1504216800
+
 baseUrl = 'https://echelog.com/logs/browse/angularjs/'
+initialUrl = ['1477954800','1475272800','1480546800','1483225200','1485903600','1488322800','1490997600','1493589600','1496268000','1498860000','1501538400','1504216800']
 extraUrl = []
 url = []
 
-#Putting the extraUrls into the extraUrl list.
-opener = urllib2.build_opener()
-ourUrl = opener.open(baseUrl+'1475272800').read()
-soup = BeautifulSoup(ourUrl)
-numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-for eachA in soup.findAll('a'):
-    firstChar = (str(eachA.get('href')))[0]
-    if(firstChar in numberList):
-        extraUrl.append(eachA.get('href'))
+for eachMonth in range(0,2):
+    #Putting the extraUrls into the extraUrl list.
+    opener = urllib2.build_opener()
+    ourUrl = opener.open(baseUrl+initialUrl[eachMonth]).read()
+    soup = BeautifulSoup(ourUrl)
+    numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for eachA in soup.findAll('a'):
+        firstChar = (str(eachA.get('href')))[0]
+        if(firstChar in numberList):
+            extraUrl.append(eachA.get('href'))
 
-#Editing the extraUrl list.
-for i in range(0,3):
-    extraUrl.pop(0)
-extraUrl.pop()
-del extraUrl[32:]
-extraUrl.pop()
+    #Editing the extraUrl list.
+    #Months with 31 days
+    if eachMonth==0 or eachMonth==2 or eachMonth==3 or eachMonth==5 or eachMonth==7 or eachMonth==9 or eachMonth==10:
+        for i in range(0,3):
+            extraUrl.pop(0)
+        extraUrl.pop()
+        del extraUrl[32:]
+        extraUrl.pop()
 
-#Populating url list.
-for i in range(0,len(extraUrl)):
-    url.append(baseUrl + extraUrl[i])
+    #Months with 30 days
+    if eachMonth==1 or eachMonth==6 or eachMonth==8 or eachMonth==11:
+        for i in range(0,3):
+            extraUrl.pop(0)
+        extraUrl.pop()
+        del extraUrl[31:]
+        extraUrl.pop()
 
-#Calling urlContentParser
-urlContentParser.urlContentParser(url)
+    #Month with 28 days
+    if eachMonth==4:
+        for i in range(0,3):
+            extraUrl.pop(0)
+        extraUrl.pop()
+        del extraUrl[32:]
+        extraUrl.pop()
+
+    #Populating url list.
+    for i in range(0,len(extraUrl)):
+        url.append(baseUrl + extraUrl[i])
+
+    #Calling urlContentParser
+    urlContentParser.urlContentParser(url, eachMonth)
+
+    #Resetting extraUrl to empty.
+    extraUrl = []
