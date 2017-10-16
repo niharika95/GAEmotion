6cdc6c7ed4d4284e24bb554566667584a728e698
Niharika <niharika13dalal@gmail.com>
2017-10-16 12:25:34 -0400

    Months with 28 days and 31 days work fine. 30 days to be fixed.

--- urlParser.py
+++ urlParser.py
@@ -3,30 +3,30 @@ from BeautifulSoup import BeautifulSoup
 import urlContentParser
 
 # 2016-17
-# October - 1475272800
-# November - 1477954800
-# December - 1480546800
-# January - 1483225200
-# February - 1485903600
-# March - 1488322800
-# April - 1490997600
-# May - 1493589600
-# June - 1496268000
-# July - 1498860000
-# August - 1501538400
-# September - 1504216800
+# 1. October - 1475272800
+# 2. November - 1477954800
+# 3. December - 1480546800
+# 4. January - 1483225200
+# 5. February - 1485903600
+# 6. March - 1488322800
+# 7. April - 1490997600
+# 8. May - 1493589600
+# 9. June - 1496268000
+# 10. July - 1498860000
+# 11. August - 1501538400
+# 12. September - 1504216800
 
 baseUrl = 'https://echelog.com/logs/browse/angularjs/'
-initialUrl = ['1477954800','1475272800','1480546800','1483225200','1485903600','1488322800','1490997600','1493589600','1496268000','1498860000','1501538400','1504216800']
+initialUrl = ['1475272800','1477954800','1480546800','1483225200','1485903600','1488322800','1490997600','1493589600','1496268000','1498860000','1501538400','1504216800']
 extraUrl = []
 url = []
 
-for eachMonth in range(0,2):
+for eachMonth in range(1,2):
     #Putting the extraUrls into the extraUrl list.
     opener = urllib2.build_opener()
     ourUrl = opener.open(baseUrl+initialUrl[eachMonth]).read()
     soup = BeautifulSoup(ourUrl)
-    numberList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    numberList = ['0', '1', '2', '3', '4', '5', '7', '7', '8', '9']
     for eachA in soup.findAll('a'):
         firstChar = (str(eachA.get('href')))[0]
         if(firstChar in numberList):
@@ -54,7 +54,7 @@ for eachMonth in range(0,2):
         for i in range(0,3):
             extraUrl.pop(0)
         extraUrl.pop()
-        del extraUrl[32:]
+        del extraUrl[29:]
         extraUrl.pop()
 
     #Populating url list.
